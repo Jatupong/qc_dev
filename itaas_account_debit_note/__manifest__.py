9d127935949faef149d09437b7dbc4b0f3b74843
peerapat <itaas.dev02@gmail.com>
2023-08-25 10:22:11 +0700

    ByPooh:task:T230812868Update:itaas_print_invoice_report

---
+++ itaas_account_debit_note/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020-today ITAAS (Dev K.Book)
+{
+    "name": "Itaas Account Debit Note",
+    "author": "ITAAS",
+    "version": "16.0.0.0",
+    "category": "mrp",
+    "website": "www.itaas.co.th",
+    "depends": ['account',],
+    "data": [
+        # 'views/sale_order_view.xml',
+        # 'views/stock_picking_view.xml',
+        'views/account_debit_note_menu_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ itaas_account_debit_note/models/account_move.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020-today ITAAS (Dev K.Book)
+
+from odoo import fields, api, models, _
+from odoo.exceptions import UserError, AccessError
+
+
+class AccountMoveseq(models.Model):
+    _inherit = 'account.move'
+
+    is_debit_note = fields.Boolean(string='Is Debit Note')
+    is_debit_invoice = fields.Boolean(string='Is Debit Invoice')
+    is_debit_vendor = fields.Boolean(string='Is Debit Vendor')
+    is_credit_note = fields.Boolean(string='Is Credit Note')
+
+
+    def _get_sequence(self):
+        ''' Return the sequence to be used during the post of the current move.
+        :return: An ir.sequence record or False.
+        '''
+
+        self.ensure_one()
+        journal = self.journal_id
+        # res = super(AccountMoveseq, self)._get_sequence()
+        if self.move_type in ('out_invoice', 'in_invoice') and self.is_debit_note and journal.debit_sequence_id:
+            return journal.debit_sequence_id
+        if self.move_type in ('out_refund', 'in_refund') and self.is_credit_note and journal.refund_sequence_id:
+            return journal.refund_sequence_id
+        return
+
