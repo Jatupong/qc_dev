4d243c6ade7fb7dc1f1dbf944d4cc3b1f409924d
peerapat <itaas.dev02@gmail.com>
2023-08-31 11:46:31 +0700

    ByPooh:task:Update:itaas_print_inventory_report

---
+++ itaas_print_inventory_report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of IT as a Service Co., Ltd.
+# Copyright (C) 2022 (www.itaas.co.th)
+
+from . import models
+from . import wizard
---
+++ itaas_print_inventory_report/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of IT as a Service Co., Ltd.
+# Copyright (C) 2022 (www.itaas.co.th)
+
+from . import stock_count_wizard
