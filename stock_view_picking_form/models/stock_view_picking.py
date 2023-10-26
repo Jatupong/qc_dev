1b177b0ce4e8ad48c717860a7efc4a649bd8daa4
itaas.dev02 <itaas.dev02@gmail.com>
2023-10-26 09:06:19 +0700

    By:Sarawut.Ph task:T230812911 Update:stock_view_picking_form

--- stock_view_picking_form/models/stock_view_picking.py
+++ stock_view_picking_form/models/stock_view_picking.py
@@ -12,7 +12,7 @@ class StockViewPicking(models.Model):
     buyer_ref = fields.Char(string='Buyer REF')
     paymentref_no = fields.Char(string='Paymentref NO')
 
-    product_ceg_th = fields.Char(string='Product Category TH')
+    # product_ceg_th = fields.Char(string='Product Category TH')
 
     mode_of_shipment = fields.Many2one(comodel_name="mode.shipment", string="Mode Of Shipment")
     shipping = fields.Many2one(comodel_name="shipping.id", string="Shipping")
