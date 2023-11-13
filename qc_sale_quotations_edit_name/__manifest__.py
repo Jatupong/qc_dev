e45a0c65cdac5195387e8a020dc02cb01fd59836
itaas.dev02 <itaas.dev02@gmail.com>
2023-11-13 10:26:22 +0700

    By:Sarawut.Ph taskT231113384 Update:qc_sale_quotations_edit_name

---
+++ qc_sale_quotations_edit_name/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of IT as a Service Co., Ltd.
+# Copyright (C) 2023-today www.itaas.co.th (Dev K.Book)
+
+{
+    'name': 'QC. Sale Quotations Edit Name',
+    'version': '16.0.0.1',
+    'category': 'base',
+    'summary': '',
+    'description': """
+                """,
+    'author': 'IT as a Service Co., Ltd.',
+    'website': 'www.itaas.co.th',
+    'depends': ['base','sale','product'],
+    'data': [
+
+        'views/sale_order_view.xml',
+
+    ],
+    'qweb': [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
