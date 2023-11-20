cf192ac52ca21b4853aef7cb045f59f71783fcb4
itaas.dev02 <itaas.dev02@gmail.com>
2023-11-20 16:08:23 +0700

    By:Sarawut Ph. task:T231113393 Update:qc_sale_order_add_fields

---
+++ qc_sale_order_add_fields/models/sale_order.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of IT as a Service Co., Ltd.
+# Copyright (C) 2022-today www.itaas.co.th (Dev K.Book)
+
+from odoo import api, fields, models, _
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    fob = fields.Char(string='FOB')
+    shipping_mark = fields.Char(string='Shipping Mark')
+    delivery_to = fields.Char(string='Delivery To')
+    logo = fields.Char(string='Logo')
