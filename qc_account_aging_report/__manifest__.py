8705ee5a46c7e0f9b7125690e014c18023804874
root <itaas.dev02@gmail.com>
2024-01-18 15:10:18 +0700

    ByBoom:T230913053:Update:qc_account_aging_report

--- qc_account_aging_report/__manifest__.py
+++ qc_account_aging_report/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'QC. Account Aging Report',
-    'version': '16.0.0.1',
+    'version': '16.0.0.2',
     'price': 'Free',
     'currency': 'THB',
     'category': 'Account',
--- qc_account_aging_report/report/__init__.py
+++ qc_account_aging_report/report/__init__.py
@@ -4,3 +4,5 @@
 
 from . import account_ap_aging_report
 from . import account_ar_aging_report
+
+
