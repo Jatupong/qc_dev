d597e037bcb11ab69458da68a477bde539e5f39c
peerapat-p <itaas.dev02@gmail.com>
2024-01-23 08:49:18 +0700

    ByPooh:task:Update:qc_add_field_purchase_vendor

--- qc_add_field_purchase_vendor/models/purchase_add_field.py
+++ qc_add_field_purchase_vendor/models/purchase_add_field.py
@@ -18,21 +18,24 @@ class purchase_add_field(models.Model):
     # _name = "res.partner"
     _inherit = "res.partner"
 
-    title = fields.Many2one('res.partner.title',string='Title',compute='_compute_title',readonly=False,store=True, required=True)
+    # title = fields.Many2one('res.partner.title',string='Title',compute='_compute_title',readonly=False,store=True, required=True)
+    # 
+    # property_supplier_payment_term_id = fields.Many2one('account.payment.term', company_dependent=True,
+    #                                            string='Payment Terms',
+    #                                            domain="[('company_id', 'in', [current_company_id, False])]",
+    #                                            help="This payment term will be used instead of the default one for sales orders and customer invoices",
+    #                                            required = True,
+    #                                            )
+    # vat = fields.Char(string='TAX ID',
+    #                   required = True,
+    #                   )
+    # ref = fields.Char(string='Reference',
+    #                   required=True,
+    #                   )
+    # street = fields.Char('Street', compute='_compute_partner_address_values', readonly=False, store=True, required=True)
     
-    property_supplier_payment_term_id = fields.Many2one('account.payment.term', company_dependent=True,
-                                               string='Payment Terms',
-                                               domain="[('company_id', 'in', [current_company_id, False])]",
-                                               help="This payment term will be used instead of the default one for sales orders and customer invoices",
-                                               required = True,
-                                               )
-    vat = fields.Char(string='TAX ID',
-                      required = True,
-                      )
-    ref = fields.Char(string='Reference',
-                      required=True,
-                      )
-    street = fields.Char('Street', compute='_compute_partner_address_values', readonly=False, store=True, required=True)
+    # -----------------------------------------------------------------------------------------------------------------
+    
     # street2 = fields.Char('Street2', compute='_compute_partner_address_values', readonly=False, store=True, required=True)
     # zip = fields.Char('Zip', change_default=True, compute='_compute_partner_address_values', readonly=False, store=True, required=True)
     # city = fields.Char('City', compute='_compute_partner_address_values', readonly=False, store=True, required=True)
