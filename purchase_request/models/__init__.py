83de1bc0bcb223f759476c753a3e6bda340ecb21
root <itaas.dev02@gmail.com>
2024-02-13 14:49:49 +0700

    ByBoom:T240213763:Update:purchase_request

--- purchase_request/models/__init__.py
+++ purchase_request/models/__init__.py
@@ -1,16 +1,16 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 
-from . import purchase_request_allocation
-from . import orderpoint
-from . import purchase_request
-from . import purchase_request_line
-from . import stock_rule
-from . import product_template
-from . import purchase_order
-from . import stock_move
-from . import stock_move_line
-from . import purchase_request_type
-from . import purchasing_type
-from . import order_type
-from . import purchase_order_type
+# from . import purchase_request_allocation
+# from . import orderpoint
+# from . import purchase_request
+# from . import purchase_request_line
+# from . import stock_rule
+# from . import product_template
+# from . import purchase_order
+# from . import stock_move
+# from . import stock_move_line
+# from . import purchase_request_type
+# from . import purchasing_type
+# from . import order_type
+# from . import purchase_order_type
 
