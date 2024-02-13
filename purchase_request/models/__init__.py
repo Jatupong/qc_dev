5aa1324c0be08ef0f13e6442e6970eb5152904cf
root <itaas.dev02@gmail.com>
2024-02-13 14:58:05 +0700

    ByBoom:T240213763:Update:purchase_request

--- purchase_request/models/__init__.py
+++ purchase_request/models/__init__.py
@@ -8,9 +8,9 @@ from . import stock_rule
 from . import product_template
 from . import purchase_order
 from . import stock_move
-from . import stock_move_line
-from . import purchase_request_type
-from . import purchasing_type
-from . import order_type
-from . import purchase_order_type
+# from . import stock_move_line
+# from . import purchase_request_type
+# from . import purchasing_type
+# from . import order_type
+# from . import purchase_order_type
 
