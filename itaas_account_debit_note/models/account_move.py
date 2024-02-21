3c76d731ed7e1df27ab819b2ffaaece78c94b1f3
itaas.dev02 <itaas.dev02@gmail.com>
2024-02-21 18:14:55 +0700

    ByMax:task:Update:itaas_account_debit_note

--- itaas_account_debit_note/models/account_move.py
+++ itaas_account_debit_note/models/account_move.py
@@ -15,6 +15,7 @@ class AccountMoveseq(models.Model):
 
     def _get_sequence(self):
         print('mmmmmm')
+        print('_get_sequence')
         ''' Return the sequence to be used during the post of the current move.
         :return: An ir.sequence record or False.
         '''
@@ -22,8 +23,11 @@ class AccountMoveseq(models.Model):
         self.ensure_one()
         journal = self.journal_id
         # res = super(AccountMoveseq, self)._get_sequence()
+        # print('move_type',self.move_type)
+        # print('_get_sequence',self.is_debit_note)
         if self.move_type in ('out_invoice', 'in_invoice') and self.is_debit_note and journal.debit_sequence_id:
             return journal.debit_sequence_id
         if self.move_type in ('out_refund', 'in_refund') and self.is_credit_note and journal.refund_sequence_id:
             return journal.refund_sequence_id
         return
+
