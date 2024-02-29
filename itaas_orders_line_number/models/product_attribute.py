6cf9a6132c2f576af74de0c4879a2185f3a2d949
peerapat-p <itaas.dev02@gmail.com>
2024-02-29 15:18:06 +0700

    ByMax:T231013303:Update:itaas_orders_line_number

--- itaas_orders_line_number/models/product_attribute.py
+++ itaas_orders_line_number/models/product_attribute.py
@@ -1,11 +1,27 @@
-from odoo import models, fields, api
+import datetime
 
+from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 class ProductAttribute(models.Model):
     _inherit = 'product.template.attribute.line'
 
+    _order = 'date_time_add_attribute_line, number'
+
     number = fields.Integer()
     sequence = fields.Integer("Sequence", default=10)
+
+    date_time_add_attribute_line = fields.Datetime()
+
+    def create(self, vals_list):
+        for i_time,value in enumerate(vals_list):
+            value['date_time_add_attribute_line'] = fields.Datetime.now() + timedelta(milliseconds=i_time)
+
+        # print('itaas_orders_line_numberrrrrrrrrrrrrrrrrrrrrrrr',vals_list)
+        res = super(ProductAttribute,self).create(vals_list)
+
+        return res
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
