a1c24d89989628abc491c05b4b7f1bb8298c181b
peerapat-p <itaas.dev02@gmail.com>
2024-03-01 15:16:53 +0700

    ByPooh:task:Update:itaas_print_tax_report

--- itaas_print_tax_report/models/__init__.py
+++ itaas_print_tax_report/models/__init__.py
@@ -2,6 +2,7 @@
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 # from . import res_company
 from . import report_tax
+from . import report_tax2
 from . import report_pnd3
 from . import report_pnd53
 from . import account_tax
