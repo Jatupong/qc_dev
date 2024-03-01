bbadb9652e98f27f0a5902af324ef5ef57525d30
support-itaas <support@itaas.co.th>
2024-03-01 11:48:41 +0700

    up: itaas_account_debit_note

--- itaas_account_debit_note/models/account_move.py
+++ itaas_account_debit_note/models/account_move.py
@@ -8,14 +8,14 @@ from odoo.exceptions import UserError, AccessError
 class AccountMoveseq(models.Model):
     _inherit = 'account.move'
 
-    is_debit_note = fields.Boolean(string='Is Debit Note')
+    is_debit_note = fields.Boolean(string='Is Debit Note', default=False)
     is_debit_invoice = fields.Boolean(string='Is Debit Invoice')
-    is_debit_vendor = fields.Boolean(string='Is Debit Vendor')
+    is_debit_vendor = fields.Boolean(string='Is Debit Vendor', default=False)
     is_credit_note = fields.Boolean(string='Is Credit Note')
-
 
     def _get_sequence(self):
         print('mmmmmm')
+        print('_get_sequence')
         ''' Return the sequence to be used during the post of the current move.
         :return: An ir.sequence record or False.
         '''
@@ -23,9 +23,10 @@ class AccountMoveseq(models.Model):
         self.ensure_one()
         journal = self.journal_id
         # res = super(AccountMoveseq, self)._get_sequence()
+        # print('move_type',self.move_type)
+        # print('_get_sequence',self.is_debit_note)
         if self.move_type in ('out_invoice', 'in_invoice') and self.is_debit_note and journal.debit_sequence_id:
             return journal.debit_sequence_id
         if self.move_type in ('out_refund', 'in_refund') and self.is_credit_note and journal.refund_sequence_id:
             return journal.refund_sequence_id
         return
-
