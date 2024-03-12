051184ee754732dca4458641993c10af3629faf1
itaas.dev02 <itaas.dev02@gmail.com>
2024-03-12 16:28:27 +0700

    By:Sarawut Ph. Task:T240313935 Update:qc_add_field_product_inventory

--- qc_add_field_product_inventory/models/product_add_field.py
+++ qc_add_field_product_inventory/models/product_add_field.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of IT as a Service Co., Ltd.
 # Create By Sarawut.Ph [D:12|M:03|Y:2024]
-
 from odoo import fields, models, _,api
 
 class ProductTemplate(models.Model):
