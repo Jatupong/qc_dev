a7a42dc4ea53533a29225d5fc3a457c516fa8f3b
itaas.dev02 <itaas.dev02@gmail.com>
2024-03-13 18:31:52 +0700

    By:Sarawut Ph. Task:T240313911 Update:qc_add_field_product_inventory

--- qc_add_field_product_inventory/models/sale_order_add_field.py
+++ qc_add_field_product_inventory/models/sale_order_add_field.py
@@ -9,20 +9,28 @@ from odoo import fields, models, _,api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    qty_per_carton = fields.Float('QTY per Carton', digits=(16, 2),related="product_template_id.qty_per_carton")
-    qty_of_carton = fields.Float('QTY of Carton', digits=(16, 2),related="product_template_id.qty_of_carton")
-    width = fields.Float(string='Width',related="product_template_id.breadth")
-    length = fields.Float(string='Length',related="product_template_id.length")
-    height = fields.Float(string='Height',related="product_template_id.height")
-    g_w_kgs = fields.Float(related="product_template_id.gross")
-    n_w_kgs = fields.Float(related="product_template_id.weight")
+    qty_per_carton = fields.Float('QTY per Carton', digits=(16, 2))
+    qty_of_carton = fields.Float('QTY of Carton', digits=(16, 2))
+
+    # qty_per_carton = fields.Float('QTY per Carton', digits=(16, 2),related="product_template_id.qty_per_carton")
+    # qty_of_carton = fields.Float('QTY of Carton', digits=(16, 2),related="product_template_id.qty_of_carton")
+    # width = fields.Float(string='Width',related="product_template_id.breadth")
+    # length = fields.Float(string='Length',related="product_template_id.length")
+    # height = fields.Float(string='Height',related="product_template_id.height")
+    # g_w_kgs = fields.Float(related="product_template_id.gross")
+    # n_w_kgs = fields.Float(related="product_template_id.weight")
 
 
-    # @api.onchange('product_template_id')
-    # def get_volume(self):
-    #     if self.product_template_id:
-    #         self.update({
-    #             'width':float(self.product_template_id.breadth) or 0,
-    #             'length': float(self.product_template_id.length) or 0,
-    #             'height': float(self.product_template_id.height) or 0,
-    #         })
+    @api.onchange('product_template_id')
+    def get_volume(self):
+        if len(self.product_template_id)>0:
+            self.update({
+                'width':float(self.product_template_id.breadth) or 0,
+                'length': float(self.product_template_id.length) or 0,
+                'height': float(self.product_template_id.height) or 0,
+                'qty_per_carton': float(self.product_template_id.qty_per_carton) or 0,
+                'qty_of_carton': float(self.product_template_id.qty_of_carton) or 0,
+                'g_w_kgs': float(self.product_template_id.gross) or 0,
+                'n_w_kgs': float(self.product_template_id.weight) or 0,
+            })
+
