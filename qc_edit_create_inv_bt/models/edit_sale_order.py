a76fcc2d4e9efd491c10e9a3cda0774b0dad1724
itaas.dev02 <itaas.dev02@gmail.com>
2024-03-26 13:34:10 +0700

    By:Sarawut Ph. Task:T240314028 Update:qc_edit_create_inv_bt

--- qc_edit_create_inv_bt/models/edit_sale_order.py
+++ qc_edit_create_inv_bt/models/edit_sale_order.py
@@ -2,7 +2,6 @@
 # Part of IT as a Service Co., Ltd.
 # Create By Sarawut.Ph [D:26|M:03|Y:2024]
 from odoo import fields, models, _,api
-
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
