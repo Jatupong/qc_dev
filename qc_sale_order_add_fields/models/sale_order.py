3d33ae900dec13dbc6a65aaa6a528a567dfe26d4
itaas.dev02 <itaas.dev02@gmail.com>
2024-03-26 14:46:37 +0700

    By:Sarawut Ph. Task:T240314033 Update:qc_sale_order_add_fields

--- qc_sale_order_add_fields/models/sale_order.py
+++ qc_sale_order_add_fields/models/sale_order.py
@@ -11,4 +11,13 @@ class SaleOrder(models.Model):
     shipping_mark = fields.Char(string='Shipping Mark')
     delivery_to = fields.Char(string='Delivery To')
     logo = fields.Char(string='Logo')
-    delivery_week = fields.Date(string='Delivery Week')
+    delivery_week = fields.Date(string='Delivery Week Date')
+    delivery_week_year = fields.Char(string='Delivery Week')
+
+    @api.onchange('delivery_week')
+    def update_delivery_week(self):
+        for sale in self:
+            if sale.delivery_week != False:
+                sale.delivery_week_year = "W{}".format(sale.delivery_week.isocalendar().week)
+            else:
+                sale.delivery_week_year = "Week"
