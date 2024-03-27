d7989c40d143d57fd0875abe651280855755e7fa
peerapat-p <itaas.dev02@gmail.com>
2024-03-27 11:33:39 +0700

    ByPooh:task:Update:qc_sale_quotations_state

---
+++ qc_sale_quotations_state/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of IT as a Service Co., Ltd.
+# Copyright (C) 2023-today www.itaas.co.th (Dev K.New)
+
+{
+    'name': 'QC. Sale Quotations State',
+    'version': '16.0.0.1',
+    'category': 'base',
+    'summary': '',
+    'description': """
+                """,
+    'author': 'IT as a Service Co., Ltd.',
+    'website': 'www.itaas.co.th',
+    'depends': ['base','sale','manufacturing_production_request'],
+    'data': [
+
+
+        'views/state_view.xml',
+
+    ],
+    'qweb': [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
