571b33e8106060dc95dc8986ae74a959edd7a88b
itaas.dev02 <itaas.dev02@gmail.com>
2024-03-28 15:08:24 +0700

    By:Sarawut Ph. Task:T231113418 Update:qc_product_pricelist_item_add_tree

--- qc_product_pricelist_item_add_tree/models/sale_order_line.py
+++ qc_product_pricelist_item_add_tree/models/sale_order_line.py
@@ -10,6 +10,117 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     product_cost = fields.Float(string='ผลิตภัณท์', compute='update_product_cost_by_set_line')
+    commission_cost = fields.Float(string='คอม',compute='update_commission_cost_by_set_line')
+    box_cost = fields.Float(string='กล่อง',compute='update_box_cost_by_set_line')
+    sticker_cost = fields.Float(string='สติ๊กเกอร์',compute='update_sticker_cost_by_set_line')
+    cost_1 = fields.Float(string='ราคา1',compute='update_cost_1_by_set_line')
+    cost_2 = fields.Float(string='ราคา2',compute='update_cost_2_by_set_line')
+    cost_3 = fields.Float(string='ราคา3',compute='update_cost_3_by_set_line')
+
+
+    def update_cost_3_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.cost_3
+                    line.update({
+                        'cost_3':zum
+                    })
+                else:
+                    line.update({
+                        'cost_3': 0
+                    })
+            else:
+                pass
+    def update_cost_2_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.cost_2
+                    line.update({
+                        'cost_2':zum
+                    })
+                else:
+                    line.update({
+                        'cost_2': 0
+                    })
+            else:
+                pass
+    def update_cost_1_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.cost_1
+                    line.update({
+                        'cost_1':zum
+                    })
+                else:
+                    line.update({
+                        'cost_1': 0
+                    })
+            else:
+                pass
+    def update_sticker_cost_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.sticker_cost
+                    line.update({
+                        'sticker_cost':zum
+                    })
+                else:
+                    line.update({
+                        'sticker_cost': 0
+                    })
+            else:
+                pass
+    def update_box_cost_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.box_cost
+                    line.update({
+                        'box_cost':zum
+                    })
+                else:
+                    line.update({
+                        'box_cost': 0
+                    })
+            else:
+                pass
+
+    def update_commission_cost_by_set_line(self):
+        for line in self:
+            if line.order_id:
+                if len(line.order_id.sale_order_set_line_ids)>0:
+                    zum = 0
+                    set_line = line.order_id.sale_order_set_line_ids.filtered(lambda x: x.product_set_id.id == line.product_id.id)
+                    for set in set_line:
+                        zum+= set.commission_cost
+                    line.update({
+                        'commission_cost':zum
+                    })
+                else:
+                    line.update({
+                        'commission_cost': 0
+                    })
+            else:
+                pass
     def update_product_cost_by_set_line(self):
         for line in self:
             if line.order_id:
